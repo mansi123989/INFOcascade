a2121084f83c860eb7364db4b6ff559a1bace1be
mansi123989 <parkhymansi@gmail.com>
2025-10-31 12:05:05 +0530

    Update settings.py

--- infowebapp/settings.py
+++ infowebapp/settings.py
@@ -151,11 +151,13 @@ STATICFILES_DIRS = [
 
 
 
+import os
+
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_USE_SSL = False
-EMAIL_HOST_USER = 'parkhymansi@gmail.com'
-EMAIL_HOST_PASSWORD = 'brqg btup ryik hygw'
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
